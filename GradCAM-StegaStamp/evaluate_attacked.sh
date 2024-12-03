b0f705f201019956f9d5c768e738f42e370a6d2d
rmdluo <rmdluo@gmail.com>
2024-12-03 10:11:04 -0500

    update evaluate bash script to take in cmd line params

--- GradCAM-StegaStamp/evaluate_attacked.sh
+++ GradCAM-StegaStamp/evaluate_attacked.sh
@@ -1,10 +1,15 @@
+stegastamp_model = $1
+attacked_dir = $2
+attacked_subfolder_name = $3
+unwatermarked_dir = $4
+
 for i in $(seq 0 25 75);
 do
     for j in 5 11 31;
     do
         echo $i $j;
-        python calculate_accuracy.py ../../StegaStamp/saved_models/test/ \
-            --images_dir randomized_attacked/randomized_attacked_${i}_${j}/ 2>/dev/null | grep "Detection Rate";
-        python -m pytorch_fid randomized_attacked/randomized_attacked_${i}_${j}/ ../../StegaStamp/data/unwatermarked | grep "FID";
+        python calculate_accuracy.py $stegastamp_model \
+            --images_dir ${attacked_dir}/${attacked_subfolder_name}_${i}_${j}/ 2>/dev/null | grep "Detection Rate";
+        python -m pytorch_fid ${attacked_dir}/${attacked_subfolder_name}_${i}_${j}/ $unwatermarked_dir | grep "FID";
     done
 done
