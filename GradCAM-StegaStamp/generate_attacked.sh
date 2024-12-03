1e680224512167bcebce5be83020c19744d9855f
rmdluo <rmdluo@gmail.com>
2024-12-03 10:23:28 -0500

    add command line args

--- GradCAM-StegaStamp/generate_attacked.sh
+++ GradCAM-StegaStamp/generate_attacked.sh
@@ -1,35 +1,63 @@
-# for i in $(seq 0 25 75);
-# do
-#     for j in 5 11 31;
-#     do
-#         echo $i $j
-#         python attack_gradcam.py ../../StegaStamp/saved_models/test/ --wm_images_dir ../../StegaStamp/data/watermarked_Stega\!\!/ --gradcam_results_dir gradcams/ --output_dir gradcam/gradcam --percentile_threshold $i --blur_size $j
-#     done
-# done
+attack = $1
 
-# for i in $(seq 0 25 75);
-# do
-#     for j in 5 11 31;
-#     do
-#         echo $i $j
-#         python attack_randomized.py --wm_images_dir ../../StegaStamp/data/watermarked_Stega\!\!/ --output_dir randomized_attacked/randomized_attacked --percentile_threshold $i --blur_size $j
-#     done
-# done
-
-for i in $(seq 0 25 75)
-do
-    for j in 5 11 31
+if [$attack -eq "gradcam"];
+then
+    for i in $(seq 0 25 75);
     do
-        echo $i $j
-        python attack_residuals.py --wm_images_dir ../../StegaStamp/data/watermarked_Stega\!\!/ --unwm_images_dir ../../StegaStamp/data/unwatermarked --output_dir residuals_attacked/residuals_attacked --percentile_threshold $i --blur_size $j
+        for j in 5 11 31;
+        do
+            # $2 -- path to saved stegastamp model
+            echo $i $j
+            python attack_gradcam.py $2 \
+                                --wm_images_dir $3 \
+                                --gradcam_results_dir $4 \
+                                --output_dir gradcam_attacked/gradcam_attacked \
+                                --percentile_threshold $i \
+                                --blur_size $j
+        done
     done
-done
+fi
 
-for i in $(seq 0 25 75)
-do
-    for j in 5 11 31
+if [$attack -eq "randomized"];
+then
+    for i in $(seq 0 25 75);
     do
-        echo $i $j
-        python attack_residuals.py --wm_images_for_residuals_dir ../../StegaStamp/data/watermarked_Hello --wm_images_dir ../../StegaStamp/data/watermarked_Stega\!\!/ --unwm_images_dir ../../StegaStamp/data/unwatermarked --output_dir residuals_attacked_diff_message/residuals_attacked_diff_message --percentile_threshold $i --blur_size $j
+        for j in 5 11 31;
+        do
+            echo $i $j
+            python attack_randomized.py --wm_images_dir $2 \
+                                        --gradcams_dir $3 \
+                                        --output_dir randomized_attacked/randomized_attacked \
+                                        --percentile_threshold $i \
+                                        --blur_size $j
+        done
     done
-done
+fi
+
+if [$attack -eq "residuals"];
+then
+    for i in $(seq 0 25 75)
+    do
+        for j in 5 11 31
+        do
+            echo $i $j
+            python attack_residuals.py --wm_images_dir $2 \
+                                       --unwm_images_dir $3 \
+                                       --output_dir residuals_attacked/residuals_attacked \
+                                       --percentile_threshold $i \
+                                       --blur_size $j
+        done
+    done
+fi
+
+if [$attack -eq "residuals_diff_message"];
+then
+    for i in $(seq 0 25 75)
+    do
+        for j in 5 11 31
+        do
+            echo $i $j
+            python attack_residuals.py --wm_images_for_residuals_dir ../../StegaStamp/data/watermarked_Hello --wm_images_dir ../../StegaStamp/data/watermarked_Stega\!\!/ --unwm_images_dir ../../StegaStamp/data/unwatermarked --output_dir residuals_attacked_diff_message/residuals_attacked_diff_message --percentile_threshold $i --blur_size $j
+        done
+    done
+fi
